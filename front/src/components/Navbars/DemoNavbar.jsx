280f3329e8ab024bf8e8b8eba625c801b1371256
ghazouani <ghazouani.ilhem@gmail.com>
2020-01-26 14:35:36 +0100

    update heroku

--- front/src/components/Navbars/DemoNavbar.jsx
+++ front/src/components/Navbars/DemoNavbar.jsx
@@ -40,7 +40,12 @@ class DemoNavbar extends React.Component {
           >
             <Container>
               <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
-              
+                <Link to="/">
+                      <img
+                  alt="..."
+                  src={require("../../assets/img/brand/argon-react-white.png")}
+                />
+                      </Link>
               </NavbarBrand>
               <button className="navbar-toggler" id="navbar_global">
                 <span className="navbar-toggler-icon" />
@@ -50,7 +55,10 @@ class DemoNavbar extends React.Component {
                   <Row>
                     <Col className="collapse-brand" xs="6">
                       <Link to="/">
-                        Home
+                      <img
+                  alt="..."
+                  src={require("../../assets/img/brand/argon-react-white.png")}
+                />
                       </Link>
                     </Col>
                     <Col className="collapse-close" xs="6">
